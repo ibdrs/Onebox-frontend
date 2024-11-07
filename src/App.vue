81f54f68af8d4e72c9f9f6c4ecd3825f964fba2b
Stef Robins <schrobins110@gmail.com>
2024-11-07 10:36:21 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Home from './components/HomePage.vue'
+</script>
+
+<template>
+  <main>
+    <Home />
+  </main>
+</template>
+
+<style lang="scss">
+  @use "./assets/main.scss";
+</style>
