4b817321aa5ccdcf2af5008b39dccc3d98bbb123
ibdrs <ivanbogdanovic677@gmail.com>
2025-01-04 14:47:25 +0100

    Updated dashboard to use API requests

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,35 +1,56 @@
 <script setup>
+  import { ref, onMounted } from 'vue';
   import Header from '../components/Header.vue';
   import Footer from '../components/Footer.vue';
-  import { ref } from 'vue';
 
-  const stateOpened = ref(false);
+  const stateLocked = ref(true);
+
+  const user = JSON.parse(localStorage.getItem('user'));
+  const userID = user.data.customerID;
+
+  const fetchBoxState = async () => {
+    try {
+      const response = await fetch(`https://localhost:7230/api/Box/${userID}/state`, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+        },
+      });
+
+      const state = await response.json();
+      stateLocked.value = state === 'locked';
+    } catch (error) {
+      console.error('Error fetching box state:', error);
+    }
+  };
 
   const unlockBox = () => {
-    //let user = localStorage.get('user');
+    //let user = localStorage.getItem('user');
     //console.log(user);
-    return fetch('https://192.168.152.36/onebox_api/box/unlock/1', {
-      method: 'GET',
+    return fetch(`https://localhost:7230/api/Box/${userID}/unlock`, {
+      method: 'POST',
       headers: {
         'content-type': 'application/json'
       }
     })
-    .then(() => {
-      stateOpened.value = true;
+    .then((data) => {
+      stateLocked.value = true;
+      console.log('Box unlocked:', data);
     });
   };
 
   const lockBox = () => {
-    //let user = localStorage.get('user');
+    //let user = localStorage.getItem('user');
     //console.log(user);
-    return fetch('https://192.168.152.36/onebox_api/box/lock/1', {
-      method: 'GET',
+    return fetch(`https://localhost:7230/api/Box/${userID}/lock`, {
+      method: 'POST',
       headers: {
         'content-type': 'application/json'
       }
     })
-    .then(() => {
-      stateOpened.value = false;
+    .then((data) => {
+      stateLocked.value = false;
+      console.log('Box locked:', data);
     });
   };
 </script>
@@ -48,6 +69,10 @@
       }
     }
   };
+
+  onMounted(() => {
+    fetchBoxState();
+  });
 </script>
 <template>
   <Header />
@@ -55,8 +80,8 @@
   <main class="page_container">
     <div class="page_inner_container">
       <h2>Onebox</h2>
-      <button @click="unlockBox" :disabled="stateOpened">Open Onebox</button>
-      <button @click="lockBox" :disabled="!stateOpened">Sluit Onebox</button>
+      <button @click="unlockBox" :disabled="stateLocked">Open Onebox</button>
+      <button @click="lockBox" :disabled="!stateLocked">Sluit Onebox</button>
       <!-- Insert the LogoutButton component -->
     </div>
   </main>
