4804687b17af2e9596b0753f9959f870b6b7413b
ibdrs <ivanbogdanovic677@gmail.com>
2025-01-14 10:49:35 +0100

    - Added frontend authentication

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,38 +1,17 @@
+import axios from "axios";
+
 class AuthService {
   login(user) {
-    return new Promise((resolve, reject) => {
-
-      const mockUsers = [
-        { username: "ivan", password: "test123", customerID: 1 },
-        { username: "admin", password: "adminpass", customerID: 2 }
-      ];
-
-
-      const foundUser = mockUsers.find(
-        (mockUser) => mockUser.username === user.username && mockUser.password === user.password
-      );
-
-      if (foundUser) {
-
-        const tokenPayload = {
-          username: foundUser.username,
-          customerID: foundUser.customerID
-        };
-        const mockAccessToken = btoa(JSON.stringify(tokenPayload)); // Base64-encoded mock token
-
-        const responseData = {
-          accessToken: mockAccessToken,
-          data: tokenPayload
-        };
-
-
-        localStorage.setItem("user", JSON.stringify(responseData));
-        resolve(responseData);
-      } else {
-
-        reject(new Error("Invalid username or password"));
-      }
-    });
+    return axios.post("https://localhost:7230/api/Auth/login", {
+      username: user.username,
+      password: user.password,
+    })
+      .then(response => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        return response.data;
+      });
   }
 
   logout() {
